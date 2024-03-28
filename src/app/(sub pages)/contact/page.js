f6cd218aa6cbee6af365a203b947c5e06e6d2b0a
RAJA LAKSHMI R <127002476+RajalakshmiR24@users.noreply.github.com>
2024-03-28 10:15:59 +0530

    Update page.js

--- src/app/(sub pages)/contact/page.js
+++ src/app/(sub pages)/contact/page.js
@@ -20,18 +20,28 @@ export default function Contact() {
       <article className="relative w-full flex flex-col items-center justify-center py-8 sm:py-0 space-y-8">
         <div className="flex flex-col items-center justify-center space-y-6 w-full sm:w-3/4">
           <h1 className="text-accent font-semibold text-center text-4xl capitalize">
-            summon the wizard
+            Contact ME
           </h1>
-          <p className="text-center font-light text-sm xs:text-base">
-            Step into the circle of enchantment and weave your words into the
-            fabric of the cosmos. Whether you seek to conjure collaborations,
-            unlock mysteries, or simply share tales of adventure, your messages
-            are treasured scrolls within this realm. Use the form below to send
-            your missives through the ethereal network, and await the whisper of
-            magic in response.
-          </p>
         </div>
         <Form />
+        <div className="flex space-x-4">
+          <a
+            href="https://wa.link/ui5mwk"
+            className="footer__social"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <i className="uil uil-whatsapp"></i>
+          </a>
+          <a
+            href="https://www.instagram.com/rajalakshmi_official_24"
+            className="footer__social"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <i className="uil uil-instagram"></i>
+          </a>
+        </div>
       </article>
     </>
   );
