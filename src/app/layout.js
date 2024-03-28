ac44a82476ee8bd995d36b5a64224e5fc6e99fa3
RAJA LAKSHMI R <127002476+RajalakshmiR24@users.noreply.github.com>
2024-03-28 10:32:16 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,12 +12,12 @@ const inter = Inter({
 export const metadata = {
   title: {
     template:
-      "Next.js Portfolio Created with Three.js and Tailwind CSS | %s | CodeBucks",
+      "Rajalakshmi Portfolio Created with Three.js and Tailwind CSS",
     default:
-      "Next.js Portfolio Created with Three.js and Tailwind CSS by CodeBucks",
+      "Rajalakshmi Portfolio Created with Three.js and Tailwind CSSs",
   },
   description:
-    "A unique creative portfolio designed by CodeBucks with cutting-edge technologies like Next.js, Tailwind CSS, Three.js, and Framer Motion. Experience the art of modern web development firsthand. Checkout CodeBucks on youtube.",
+    "A unique creative portfolio designed with cutting-edge technologies like Next.js, Tailwind CSS, Three.js, and Framer Motion. Experience the art of modern web development firsthand.",
 };
 
 export default function RootLayout({ children }) {
